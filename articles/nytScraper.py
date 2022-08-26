aa51d96068cf4ef664a0553f476563c0f5a47d17
cmese <99047840+cmese@users.noreply.github.com>
2022-08-26 19:29:12 -0400

    changes how nytScraper gets categories (processes url instead of feed tags now)

--- articles/nytScraper.py
+++ articles/nytScraper.py
@@ -17,15 +17,38 @@ def getArticles():
                 article["description"] = item.summary
                 article["url"] = item.link
                 article["imageURL"] = item.media_content[0]['url']
+                category = processURL(item.link)
+                if category:
+                    category_set = set()
+                    category_set.add(category)
+                    article["categories"] = category_set
+                else:
+                    continue
+                '''
                 category_set = set()
                 for category in item.tags:
-                    category_set.add(category)
+                    category_set.add(category.term)
                 if (category_set is set()):
                     continue
                 article["categories"] = category_set
+                '''
             except (AttributeError, KeyError):
                 continue
             articles.append(Article.from_dict(article))
     return [*set(articles)]  # removes duplicates
+
+def processURL(url):
+    splitURL = url.split("/")
+    if len(splitURL) >= 6:
+        category = splitURL[6]
+        return category
+    return
+
+def processCategory(category):
+    split_category = category.split("/")
+    if split_category[0] == "fox-news":
+        if split_category[1] in fox_categories:
+            return fox_categories[split_category[1]]
+    return
 
 print(getArticles())
